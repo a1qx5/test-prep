fbd5a9dd99c8a3600343ab26d14d242e9bb9133c
Nanulescu A Alexandru <nanulescualexandru23@stud.ase.ro>
2025-04-22 13:51:45 +0300

    upload project
    just ui

---
+++ ParticipantManager/MainForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace ParticipantManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.binarySerializationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serializaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xMLSerializationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serializeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deserializeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbNewParticipant = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.gbNewParticipant.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.binarySerializationToolStripMenuItem,
+            this.xMLSerializationToolStripMenuItem,
+            this.teToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(835, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // binarySerializationToolStripMenuItem
+            // 
+            this.binarySerializationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serializaToolStripMenuItem,
+            this.dToolStripMenuItem});
+            this.binarySerializationToolStripMenuItem.Name = "binarySerializationToolStripMenuItem";
+            this.binarySerializationToolStripMenuItem.Size = new System.Drawing.Size(118, 20);
+            this.binarySerializationToolStripMenuItem.Text = "Binary Serialization";
+            // 
+            // serializaToolStripMenuItem
+            // 
+            this.serializaToolStripMenuItem.Name = "serializaToolStripMenuItem";
+            this.serializaToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.serializaToolStripMenuItem.Text = "Serialize";
+            this.serializaToolStripMenuItem.Click += new System.EventHandler(this.serializaToolStripMenuItem_Click);
+            // 
+            // dToolStripMenuItem
+            // 
+            this.dToolStripMenuItem.Name = "dToolStripMenuItem";
+            this.dToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.dToolStripMenuItem.Text = "Deserialize";
+            // 
+            // xMLSerializationToolStripMenuItem
+            // 
+            this.xMLSerializationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serializeToolStripMenuItem,
+            this.deserializeToolStripMenuItem});
+            this.xMLSerializationToolStripMenuItem.Name = "xMLSerializationToolStripMenuItem";
+            this.xMLSerializationToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.xMLSerializationToolStripMenuItem.Text = "XML Serialization";
+            // 
+            // serializeToolStripMenuItem
+            // 
+            this.serializeToolStripMenuItem.Name = "serializeToolStripMenuItem";
+            this.serializeToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.serializeToolStripMenuItem.Text = "Serialize";
+            // 
+            // deserializeToolStripMenuItem
+            // 
+            this.deserializeToolStripMenuItem.Name = "deserializeToolStripMenuItem";
+            this.deserializeToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.deserializeToolStripMenuItem.Text = "Deserialize";
+            // 
+            // teToolStripMenuItem
+            // 
+            this.teToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem});
+            this.teToolStripMenuItem.Name = "teToolStripMenuItem";
+            this.teToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.teToolStripMenuItem.Text = "TextFile";
+            // 
+            // gbNewParticipant
+            // 
+            this.gbNewParticipant.Controls.Add(this.btnAdd);
+            this.gbNewParticipant.Controls.Add(this.dtpBirthDate);
+            this.gbNewParticipant.Controls.Add(this.tbFirstName);
+            this.gbNewParticipant.Controls.Add(this.tbLastName);
+            this.gbNewParticipant.Controls.Add(this.lblBirthDate);
+            this.gbNewParticipant.Controls.Add(this.lblFirstName);
+            this.gbNewParticipant.Controls.Add(this.lblLastName);
+            this.gbNewParticipant.Location = new System.Drawing.Point(13, 49);
+            this.gbNewParticipant.Name = "gbNewParticipant";
+            this.gbNewParticipant.Size = new System.Drawing.Size(810, 152);
+            this.gbNewParticipant.TabIndex = 1;
+            this.gbNewParticipant.TabStop = false;
+            this.gbNewParticipant.Text = "New Participant";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Silver;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.Location = new System.Drawing.Point(524, 92);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(263, 46);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add Participant";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Location = new System.Drawing.Point(587, 40);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirthDate.TabIndex = 5;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbFirstName.Location = new System.Drawing.Point(77, 78);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(188, 20);
+            this.tbFirstName.TabIndex = 4;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbLastName.Location = new System.Drawing.Point(77, 40);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(188, 20);
+            this.tbLastName.TabIndex = 3;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(521, 46);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(60, 13);
+            this.lblBirthDate.TabIndex = 2;
+            this.lblBirthDate.Text = "Birth Date: ";
+            this.lblBirthDate.Click += new System.EventHandler(this.serializaToolStripMenuItem_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(7, 78);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(63, 13);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "First Name: ";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(7, 40);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(64, 13);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name: ";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 208);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(810, 230);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Last Name";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Birth Date";
+            this.columnHeader3.Width = 120;
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.gbNewParticipant);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.gbNewParticipant.ResumeLayout(false);
+            this.gbNewParticipant.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.GroupBox gbNewParticipant;
+        private System.Windows.Forms.ToolStripMenuItem binarySerializationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serializaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xMLSerializationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serializeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deserializeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+    }
+}
+
